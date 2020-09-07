00cfcff09a7ef53de4daaa2f4d45885290e4b577
Daro Marenco <jesusmarencodev@gmail.com>
2020-09-07 14:30:30 -0500

    cambiando de build a docs

---
+++ docs/precache-manifest.fd25863339093d7aa30d4003c64f0164.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "738411a548780d2960b003561af9c39a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb0272524243748e56de",
+    "url": "/static/css/main.9d897a9b.chunk.css"
+  },
+  {
+    "revision": "4f5fe3d952a2743261a2",
+    "url": "/static/js/2.56ca0603.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56ca0603.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb0272524243748e56de",
+    "url": "/static/js/main.c679100f.chunk.js"
+  },
+  {
+    "revision": "636ee3829c54e5e01550",
+    "url": "/static/js/runtime-main.c1d890d2.js"
+  }
+]);
