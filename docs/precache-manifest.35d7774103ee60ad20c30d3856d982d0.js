48170ec9238f92f10e8275da77630627907a5b9f
Daro Marenco <jesusmarencodev@gmail.com>
2020-09-08 17:23:51 -0500

    ./ react en package.json

---
+++ docs/precache-manifest.35d7774103ee60ad20c30d3856d982d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b68bf2558cac6e2ec4d3dd32f443f63f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "62bcb3b2151ee8dc1ef0",
+    "url": "./static/css/main.9d897a9b.chunk.css"
+  },
+  {
+    "revision": "4f5fe3d952a2743261a2",
+    "url": "./static/js/2.56ca0603.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.56ca0603.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62bcb3b2151ee8dc1ef0",
+    "url": "./static/js/main.10ad1c63.chunk.js"
+  },
+  {
+    "revision": "a14f2206094bda24c3ad",
+    "url": "./static/js/runtime-main.57e3743f.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.254985adee92dafcbd28247b51665b96.js"
+  "./precache-manifest.35d7774103ee60ad20c30d3856d982d0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
